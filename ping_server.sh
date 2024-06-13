5a31f27158d749d0212ea43aaff796ec2c8178e1
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-06-13 09:14:35 +0200

    Update ping_server.sh

--- ping_server.sh
+++ ping_server.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
+
+# URL of your server's health check endpoint
+URL="https://portfolio-backend-389u.onrender.com/healthcheck"
+
+# Send a GET request to the health check endpoint
+curl -I $URL
 
